553850d7e7a38d842d1e2c4d7c070b9832d3e440
Parker Spector-Salwen <pxs584@miami.edu>
2019-09-16 13:21:09 -0400

    class

--- class-examples/p5/loadimage/sketch.js
+++ class-examples/p5/loadimage/sketch.js
@@ -9,13 +9,19 @@ puppyImage = loadImage("puppy.jpg");
 
 function setup(){
   //put swtup code here
-
+  createCanvas(400,400);
 
 }
 
 function draw(){
   //put drawing code here
-
+  //image var, x pos, y pos
+image(puppyImage,0,0);
+//image var, x pos, y pos, width, height
+//dividing the width and height by a number scales proportionally
+image(puppyImage,0,0, puppyImage.width/4,puppyImage.height/4);
+//Don't choose a random size
+image(puppyImage,200,200, 154,200);
 }
 
 //end of document
