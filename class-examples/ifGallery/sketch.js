dd649850ea14256e6c8cd7fec064f9f5e255ab50
Parker Spector-Salwen <pxs584@miami.edu>
2019-09-18 12:53:01 -0400

    gallery

--- class-examples/ifGallery/sketch.js
+++ class-examples/ifGallery/sketch.js
@@ -2,10 +2,22 @@
 // Puppy image "puppies" by www.petian.net is licensed under CC BY-NC-ND 2.0  https://search.creativecommons.org/photos/4f271583-e45d-4faf-bfc8-7291c7c1e3c3
 // dinosaur "Rupenhorn Dinosaur #4" by Kristian_Laban is licensed under CC BY-NC 2.0 https://search.creativecommons.org/photos/96213ba5-e9b6-4f2f-91e4-da28b9fcad22
 
+var puppy, dinosaur, sloth;
+
+
+
+function preload(){
+  // load media
+  puppy = loadImage("images/puppy.jpg");
+  dinosaur = loadImage("images/dinosaur.jpg");
+  sloth = loadImage("images/sloth.jpg");
+}
 function setup() {
   // put setup code here
+  createCanvas(400,400);
 }
 
 function draw() {
   // put drawing code here
+  image(puppy,0,0,puppy.width/4);
 }
