32c1f1078e66340c3d43e1e5a19a5f063bde5fe3
Parker Spector-Salwen <pxs584@miami.edu>
2019-10-02 13:09:02 -0400

    season

--- class-examples/season/gettingstarted/sketch.js
+++ class-examples/season/gettingstarted/sketch.js
@@ -1,20 +1,18 @@
-var interval= 1000;
-var prevMillis= 0;
-var counter=0;
-
+var seasonSelect;
+var seasonType= "";
 
 function setup() {
 //put setup code here
-
+createCanvas(400,400);
+seasonSelect = createSelect();
+seasonSelect.position(10,10);
+seasonSelect.option("");
+seasonSelect.option("fall");
+seasonSelect.option("winter");
+seasonSelect.option("spring");
+seasonSelect.option("summer");
 }
 
 function draw() {
   // put drawing code here
-  //console.log(millis);
-  if(millis()> interval){
-    //do something
-    prevMillis= millis();
-    counter = counter + 1;
-    console.log(counter);
-  }
 }
