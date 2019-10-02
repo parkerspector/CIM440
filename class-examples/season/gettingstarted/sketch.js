39f6e13a253c221ec205031f17abac75bb5c6129
Parker Spector-Salwen <pxs584@miami.edu>
2019-10-02 13:18:47 -0400

    season

--- class-examples/season/gettingstarted/sketch.js
+++ class-examples/season/gettingstarted/sketch.js
@@ -11,8 +11,29 @@ seasonSelect.option("fall");
 seasonSelect.option("winter");
 seasonSelect.option("spring");
 seasonSelect.option("summer");
+seasonSelect.changed(function(){
+
+  seasonType = seasonSelect.value();
+
+
+  });
+
 }
 
 function draw() {
   // put drawing code here
-}
+  background(255);
+  if(seasonType == "fall"){
+    console.log("fall");
+  }else if(seasonType == "winter"){
+    console.log("winter");
+  }else if(seasonType == "spring"){
+    console.log("spring");
+  }else if(seasonType == "summer"){
+    console.log("summer");
+    ellipse(width/2, height, width, height/2);
+  }else(
+    console.log("blank");
+    text("Make a selection", 100, 10);
+  )
+}//end of draw loop
