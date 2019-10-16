02d8cc6a9adac90a8ed029eb86cef6a3d8d66803
Parker Spector-Salwen <pxs584@miami.edu>
2019-10-16 12:50:38 -0400

    midterm

--- Homework/p5/midterm/sketch.js
+++ Homework/p5/midterm/sketch.js
@@ -1,50 +1,68 @@
-var throw, fetch, return;
-var tButton, fButton, rButton;
+var throwB, returnB;
+var tButton , rButton;
 
-var currentImage = 0;
+var currentImage = -1;
 
+var boneX = 0;
+var boneY = 400;
 
+var moveDog = false;
+
+var rectX = 0;
+var rectY= 400;
 
 
 function preload(){
-  throw = loadImage("image/dog.jpg");
-  fetch = loadImage("image/dog.jpg");
-  return = loadImage("image/dog.jpg");
+  throwB = loadImage("image/dog.jpg");
+  returnB = loadImage("image/dogbone.jpg");
 }
 
-function setup() {
   function setup() {
     // put setup code here
     createCanvas(400,400);
-    background("blue")
 
     tButton = createButton("Throw");
     tButton.mousePressed(function(){
       currentImage = 0;
     });
-    fButton = createButton("Fetch");
-    fButton.mousePressed(function(){
-      currentImage = 1;
-    });
     rButton = createButton("Return");
     rButton.mousePressed(function(){
-      currentImage = 2;
+      currentImage = 1;
     });
   }
-}
+
 function draw() {
+  background("lightblue")
+
+
   // put drawing code here
   console.log("currentImage"+ currentImage);
 
+  image(throwB,0,0,throwB.width/4,throwB.height/4);
+
   if(currentImage ==0){
-    //show puppy
-    image(throw,0,0,throw.width/4,throw.height/4);
-  }else if(currentImage ==1){
-    //show dinosaur
-      image(fetch,0,0,fetch.width/4,fetch.height/4);
-  }else if(currentImage ==2){
-    //show sloth
-      image(fetch,0,0,fetch.width/4,fetch.height/4);
+    //show dog
+
+    if(boneX < width -10 && boneY > 10){
+      boneX = boneX + 1;
+      boneY = boneY - 1;
+    }else{
+      moveDog = true;
+    }
+
+
+
+  if(currentImage ==1){
+    //show dog with bone
+    image(returnB,0,0,returnB.width/4,returnB.height/4);
+}
+  if(moveDog == true){
+    rect(200,200,20,20);
+  }else if(rectX < width -10 && rectY > 10)
+      rectX = rectX + 1;
+      rectY = rectY - 1;
   }
-  }
+
+
+  ellipse(boneX, boneY, 10,10);
 }
