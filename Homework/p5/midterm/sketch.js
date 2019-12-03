695e951c0079e2f4725b236d5f84b321c8ce3665
Parker Spector-Salwen <pxs584@miami.edu>
2019-12-03 17:22:27 -0500

    midterm

--- Homework/p5/midterm/sketch.js
+++ Homework/p5/midterm/sketch.js
@@ -49,23 +49,21 @@ function preload(){
       grassImage= 1;
     });
 
-      var container0 = createDiv();
-      container0.id("container0");
-      // # => designate an id, find an id = container0
-      //.html injects html or text into another html tag
-      select("#container0").html("<h1>Midterm</h1>");
-      //.style("css property", "value")
+    var cnv = createCanvas(400,400);
+  cnv.id("mycanvas");
 
-      select("#container0").style("width", "400px");//setting width of container0
-      select("#container0").style("margin", "20px auto");//center our container, margin: 0 auto; 0 refers to the top and bottom spacing of our container, the auto refers to the left and right spacing of our container
-      //To center our container, you need to set a width and margin: 0 auto;
+  var container0 = createDiv();
+  container0.id("container0");
+  // # => designate an id, find an id = container0
+  //.html injects html or text into another html tag
+  select("#container0").html("<h1>Fetch!</h1>");
+  //.style("css property", "value")
 
-
-    //  select("#container0").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/yPWkPOfnGsw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-
-      select('body').style("background-color","white");
-      //select('body').style("background-image", "url('assets/image.jpg')");
-    }
+  select("#container0").style("width", "400px");//setting width of container0
+  select("#container0").style("margin", "20px auto");//center our container, margin: 0 auto; 0 refers to the top and bottom spacing of our container, the auto refers to the left and right spacing of our container
+  //To center our container, you need to set a width and margin: 0 auto;
+  cnv.parent("#container0");
+}
 
 
 function draw(){
