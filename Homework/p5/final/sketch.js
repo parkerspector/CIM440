644728ea8fc8f810e028a59ca3f20c8647bb7d9f
Parker Spector-Salwen <pxs584@miami.edu>
2019-12-09 17:25:02 -0500

    final

--- Homework/p5/final/sketch.js
+++ Homework/p5/final/sketch.js
@@ -143,8 +143,6 @@ if(paddleBMove == true){
 
   text(computer, 20,205);
   text(user, 730,205);
-  <h1>Air Hockey</h1>
-  <p>This is an online air hockey game. The goal of the game is to score against the computer and prevent it from scoring on you. To score, you have to get the ball in the middle of the goal on the opposite side of the screen. To hit the ball, move the mouse and the paddle with move with it, that way you can switch the direction the ball is going. Person with the most goals in the end wins.</p>
 
 
     drawSprites();
