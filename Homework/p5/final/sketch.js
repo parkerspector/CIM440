fc290b3f6f28cf28417ea99ddec516364c6960a8
Parker Spector-Salwen <pxs584@miami.edu>
2019-12-09 16:36:40 -0500

    final

--- Homework/p5/final/sketch.js
+++ Homework/p5/final/sketch.js
@@ -33,7 +33,7 @@ function preload() {
 
 function setup() {
   createCanvas(800, 400);
-  textSize(21);
+  textSize(12);
   //frameRate(6);
 
 
@@ -144,7 +144,7 @@ if(paddleBMove == true){
   text("computer", 20,205);
   text("user", 730,205);
   text("Air Hockey",0,10);
-  text("This is an online air hockey game. The goal of the game is to score against the computer and prevent it from scoring on you. To score, you have to get the ball in the middle of the goal on the opposite side of the screen. To hit the ball, move the mouse and the paddle with move with it, that way you can switch the direction the ball is going. Person with the most goals in the end wins.",0,0);
+  text("This is an online air hockey game. The goal of the game is to score against the computer and prevent it from scoring on you. To score, you have to get the ball in the middle of the goal on the opposite side of the screen. To hit the ball, move the mouse and the paddle with move with it, that way you can switch the direction the ball is going. Person with the most goals in the end wins.",0,30);
 
     drawSprites();
 
